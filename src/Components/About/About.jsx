e998f4649656dcb65de87b9d5e0d0c3168701c31
Vipin Kumar <vipintech195504@gmail.com>
2024-10-24 12:35:30 +0530

    about-section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,12 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Banner1 from '../../assets/aboutus/about-banner.jpg'
 import Lottie from 'lottie-react'
 import Animation from '../../assets/aboutus/aboutani.json'
 import { ImCheckboxChecked } from 'react-icons/im'
-
+import { GrDocumentPerformance } from "react-icons/gr";
+import { TbMoodCrazyHappy } from "react-icons/tb";
+import { GrUserExpert } from "react-icons/gr";
+import { LiaAwardSolid } from "react-icons/lia";
 
 const About = () => {
-    
+    const [count1, setCount1] = useState(0)
+    const [count2, setCount2] = useState(0)
+    const [count3, setCount3] = useState(0)
+    const [count4, setCount4] = useState(0)
+
+
+   useEffect(() => {
+    const projectInterval = setInterval(() => {
+        setCount1(prev => {
+            if(prev >= 54){
+                clearInterval(projectInterval);
+                return prev;
+            }
+            return prev + 1;
+        })
+    }, 54);
+
+    const  projectInterval2 = setInterval(() => {
+        setCount2(prev => {
+            if(prev >= 56){
+                clearInterval*(projectInterval2);
+                return prev;
+            }
+            return prev + 1;
+        })
+    }, 56)
+
+    const projectInterval3 = setInterval(() => {
+        setCount3(prev => {
+            if(prev >= 30){
+                clearInterval(projectInterval3);
+                return prev;
+            }
+            return prev + 1;
+        })
+    }, 30)
+
+     const projectInterval4 = setInterval(() => {
+        setCount4(prev => {
+            if(prev > 130){
+                clearInterval(projectInterval4);
+                return prev;
+            }
+            return prev + 1;
+        })
+     }, 130)
+     return () => {
+        clearInterval(projectInterval);
+        clearInterval(projectInterval2);
+        clearInterval(projectInterval3);
+        clearInterval(projectInterval4);
+     }
+   })
+
     return (
         <div className='about-container'>
             <div className=''>
@@ -39,6 +95,38 @@ const About = () => {
                         </div>
                         <div className='btns'>
                             <button className='button-1 font'>Discover More</button>
+                        </div>
+                    </div>
+                </div>
+                <div className='project-completion'>
+                    <div className='project-completion-item'>
+                        <div className='project-item-list'>
+                            <GrDocumentPerformance className='icon-size'/>
+                            <div className='project-content'>
+                                <h1 className='project-content'>{count1}+</h1>
+                                <p className=' topex project-content color-1'>Project Completion</p>
+                            </div>
+                        </div>
+                        <div className='project-item-list'>
+                            <TbMoodCrazyHappy className='icon-size' />
+                            <div className='project-content'>
+                                <h1 className='project-content'>{count2}+</h1>
+                                <p className=' topex project-content color-1'>Happy Clients</p>
+                            </div>
+                        </div>
+                        <div className='project-item-list'>
+                            <GrUserExpert className='icon-size' />
+                            <div className='project-content'>
+                                <h1 className='project-content'>{count3}+</h1>
+                                <p className=' topex project-content color-1'>Skilled Expert</p>
+                            </div>
+                        </div>
+                        <div className='project-item-list'>
+                            <LiaAwardSolid className='icon-size' />
+                            <div className='project-content'>
+                                <h1 className='project-content'>{count4}+</h1>
+                                <p className=' topex project-content color-1'>Honorable Awards</p>
+                            </div>
                         </div>
                     </div>
                 </div>
